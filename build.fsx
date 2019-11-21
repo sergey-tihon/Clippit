ff71d0fa8a6db7fb38f8b101bdd8efc1bd17136e
Sergey Tihon <sergey.tihon@gmail.com>
2019-11-21 21:58:09 +0300

    NuGet package generation

--- build.fsx
+++ build.fsx
@@ -2,6 +2,9 @@
 source https://nuget.org/api/v2
 framework netstandard2.0
 nuget Fake.Core.Target
+nuget Fake.Core.ReleaseNotes 
+nuget Fake.DotNet.Paket
+nuget Fake.DotNet.AssemblyInfoFile
 nuget Fake.DotNet.Cli //"
 
 #if !FAKE
@@ -21,7 +24,26 @@ open Fake.DotNet
 open Fake.IO
 open Fake.IO.Globbing.Operators
 
+let gitName = "Clippit"
+let description = "Unofficial fork of OpenXmlPowerTools with new features"
+let release = ReleaseNotes.load "docs/RELEASE_NOTES.md"
+
 // Targets
+Target.create "Clean" (fun _ ->
+    Shell.mkdir "bin"
+    Shell.cleanDir "bin"
+)
+
+Target.create "AssemblyInfo" (fun _ ->
+    let fileName = "OpenXmlPowerTools/Properties/AssemblyInfo.Generated.cs"
+    AssemblyInfoFile.createCSharp fileName
+      [ AssemblyInfo.Title gitName
+        AssemblyInfo.Product gitName
+        AssemblyInfo.Description description
+        AssemblyInfo.Version release.AssemblyVersion
+        AssemblyInfo.FileVersion release.AssemblyVersion ]
+)
+
 Target.create "Build" (fun _ ->
     DotNet.exec id "build" "Clippit.sln -c Release" |> ignore
 )
@@ -30,11 +52,23 @@ Target.create "RunTests" (fun _ ->
     DotNet.test id "OpenXmlPowerTools.Tests/"
 )
 
+Target.create "NuGet" (fun _ ->
+    Paket.pack(fun p ->
+        { p with
+            ToolType = ToolType.CreateLocalTool()
+            OutputPath = "bin"
+            Version = release.NugetVersion
+            ReleaseNotes = String.toLines release.Notes})
+)
+
 Target.create "All" ignore
 
 // Build order
-"Build"
+"Clean"
+  ==> "AssemblyInfo"
+  ==> "Build"
   ==> "RunTests"
+  ==> "NuGet"
   ==> "All"
 
 // start build
