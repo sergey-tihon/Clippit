36f2d87d93926fdacb99314f8975f27d421fd749
Luiz Fernando Bicalho <luiz.bicalho@aglsolutions.com.br>
2022-09-20 15:28:38 -0300

    valid xml chars

--- OpenXmlPowerTools/Word/DocumentAssembler.cs
+++ OpenXmlPowerTools/Word/DocumentAssembler.cs
@@ -584,8 +584,10 @@ namespace Clippit.Word
             // From xml spec valid chars: 
             // #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]     
             // any Unicode character, excluding the surrogate blocks, FFFE, and FFFF. 
-            string re = @"[^\x09\x0A\x0D\x20-\xD7FF\xE000-\xFFFD\x10000-x10FFFF]";
-            return Regex.Replace(text, re, "");
+            //string re = @"[^\x09\x0A\x0D\x20-\xD7FF\xE000-\xFFFD\x10000-x10FFFF]";
+            //return Regex.Replace(text, re, "");
+
+            return new string(text.Where(ch => System.Xml.XmlConvert.IsXmlChar(ch)).ToArray());
         }
 
         private static XElement TransformXmlTextToMetadata(TemplateError te, string xmlText)
