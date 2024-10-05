8c90806e8f3354cb86a0aaa29a43dba2d33e4577
Sergey Tihon <sergey.tihon@gmail.com>
2024-10-05 19:02:40 +0200

    fix: simplify relId gen code

--- Clippit/Internal/Relationships.cs
+++ Clippit/Internal/Relationships.cs
@@ -4,6 +4,9 @@ namespace Clippit.Internal;
 
 internal static class Relationships
 {
-    internal static string GetNewRelationshipId() =>
-        string.Concat("rcId", Guid.NewGuid().ToString().Replace("-", "").AsSpan(0, 16));
+    internal static string GetNewRelationshipId()
+    {
+        var uid = Guid.NewGuid().ToString().Replace("-", "").AsSpan(0, 16);
+        return string.Concat("rcId", uid);
+    }
 }
