7777564a0579508da3cf5f98fcc47f283f1ecd8f
Sergey Tihon <sergey.tihon@gmail.com>
2024-10-06 15:52:24 +0200

    split builder and memory-optimized API for slides publishing

---
+++ Clippit/PowerPoint/PresentationBuilder.Fluent.cs
@@ -0,0 +1,12 @@
+﻿using Clippit.PowerPoint.Fluent;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace Clippit.PowerPoint;
+
+public static partial class PresentationBuilder
+{
+    public static IFluentPresentationBuilder Create(PresentationDocument document)
+    {
+        return new FluentPresentationBuilder(document);
+    }
+}
---
+++ Clippit/PowerPoint/PresentationBuilder.PublishSlides.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Clippit.PowerPoint.Fluent;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace Clippit.PowerPoint;
+
+public static partial class PresentationBuilder
+{
+    public static IEnumerable<PmlDocument> PublishSlides(PmlDocument src)
+    {
+        using var streamSrcDoc = new OpenXmlMemoryStreamDocument(src);
+        using var srcDoc = streamSrcDoc.GetPresentationDocument(new OpenSettings { AutoSave = false });
+        return PublishSlides(srcDoc, src.FileName);
+    }
+
+    public static IEnumerable<PmlDocument> PublishSlides(PresentationDocument srcDoc, string fileName)
+    {
+        var slideNameRegex = SlideNameRegex();
+        var slideNumber = 0;
+        foreach (var memoryStreamDocument in PublishSlides(srcDoc))
+        {
+            using var streamDoc = memoryStreamDocument;
+
+            var slideDoc = streamDoc.GetModifiedPmlDocument();
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                slideDoc.FileName = slideNameRegex.Replace(fileName, $"_{++slideNumber:000}.pptx");
+            }
+
+            yield return slideDoc;
+        }
+    }
+
+    public static IEnumerable<OpenXmlMemoryStreamDocument> PublishSlides(PresentationDocument srcDoc)
+    {
+        var slidesIds = PresentationBuilderTools.GetSlideIdsInOrder(srcDoc);
+        foreach (var slideId in slidesIds)
+        {
+            var srcSlidePart = (SlidePart)srcDoc.PresentationPart.GetPartById(slideId);
+
+            var streamDoc = OpenXmlMemoryStreamDocument.CreatePresentationDocument();
+
+            using (var output = streamDoc.GetPresentationDocument(new OpenSettings { AutoSave = false }))
+            {
+                ExtractSlide(srcSlidePart, output);
+
+                // Set the title of the new presentation to the title of the slide
+                var title = PresentationBuilderTools.GetSlideTitle(srcSlidePart.GetXElement());
+                output.PackageProperties.Title = title;
+            }
+
+            yield return streamDoc;
+        }
+    }
+
+    private static void ExtractSlide(SlidePart slidePart, PresentationDocument output)
+    {
+        using var builder = new FluentPresentationBuilder(output);
+        try
+        {
+            var newSlidePart = builder.AddSlide(slidePart);
+
+            // Remove the show attribute from the slide element (if it exists)
+            var slideDocument = newSlidePart.GetXDocument();
+            slideDocument.Root?.Attribute(NoNamespace.show)?.Remove();
+        }
+        catch (PresentationBuilderInternalException dbie)
+        {
+            if (dbie.Message.Contains("{0}"))
+                throw new PresentationBuilderException(string.Format(dbie.Message, slidePart.Uri));
+            throw;
+        }
+    }
+
+    [GeneratedRegex(".pptx", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex SlideNameRegex();
+}
