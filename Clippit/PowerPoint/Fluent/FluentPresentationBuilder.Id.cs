cf368dfd4c4639251619587a33fa1bf4a2280481
Sergey Tihon <sergey.tihon@gmail.com>
2024-10-07 22:50:24 +0200

    feat: layers for builder methods

---
+++ Clippit/PowerPoint/Fluent/FluentPresentationBuilder.Id.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Clippit.PowerPoint.Fluent;
+
+internal sealed partial class FluentPresentationBuilder
+{
+    private uint GetNextFreeId()
+    {
+        uint newId = 0;
+
+        var presentationPartDoc = _newDocument.PresentationPart.GetXDocument();
+        var masterIds = presentationPartDoc
+            .Root.Descendants(P.sldMasterId)
+            .Select(f => (uint)f.Attribute(NoNamespace.id))
+            .ToList();
+        if (masterIds.Count != 0)
+            newId = Math.Max(newId, masterIds.Max());
+
+        foreach (var slideMasterData in _slideMasters.Values)
+        {
+            var masterPartDoc = slideMasterData.Part.GetXDocument();
+            var layoutIds = masterPartDoc
+                .Root.Descendants(P.sldLayoutId)
+                .Select(f => (uint)f.Attribute(NoNamespace.id))
+                .ToList();
+            if (layoutIds.Count != 0)
+                newId = Math.Max(newId, layoutIds.Max());
+        }
+
+        return newId == 0 ? 2147483648 : newId + 1;
+    }
+}
