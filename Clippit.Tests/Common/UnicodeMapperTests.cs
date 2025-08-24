b72bc5dccac0b5a06fbd988c0429632a062e5219
Sergey Tihon <sergey.tihon@gmail.com>
2025-08-24 20:54:50 +0200

    save

--- Clippit.Tests/Common/UnicodeMapperTests.cs
+++ Clippit.Tests/Common/UnicodeMapperTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 /***************************************************************************
 
 Copyright (c) Microsoft Corporation 2016.
@@ -13,134 +12,116 @@ Developer: Thomas Barnekow
 Email: thomas@barnekow.info
 
 ***************************************************************************/
-
 using System.Xml.Linq;
-using Xunit;
 
-#if !ELIDE_XUNIT_TESTS
+namespace Clippit.Tests.Common;
 
-namespace Clippit.Tests.Common
+public class UnicodeMapperTests
 {
-    public class UnicodeMapperTests
+    [Test]
+    public async Task CanStringifyRunAndTextElements()
     {
-        [Fact]
-        public void CanStringifyRunAndTextElements()
-        {
-            const string TextValue = "Hello World!";
-            var textElement = new XElement(W.t, TextValue);
-            var runElement = new XElement(W.r, textElement);
-            var formattedRunElement = new XElement(W.r, new XElement(W.rPr, new XElement(W.b)), textElement);
+        const string TextValue = "Hello World!";
+        var textElement = new XElement(W.t, TextValue);
+        var runElement = new XElement(W.r, textElement);
+        var formattedRunElement = new XElement(W.r, new XElement(W.rPr, new XElement(W.b)), textElement);
+        await Assert.That(UnicodeMapper.RunToString(textElement)).IsEqualTo(TextValue);
+        await Assert.That(UnicodeMapper.RunToString(runElement)).IsEqualTo(TextValue);
+        await Assert.That(UnicodeMapper.RunToString(formattedRunElement)).IsEqualTo(TextValue);
+    }
 
-            Assert.Equal(TextValue, UnicodeMapper.RunToString(textElement));
-            Assert.Equal(TextValue, UnicodeMapper.RunToString(runElement));
-            Assert.Equal(TextValue, UnicodeMapper.RunToString(formattedRunElement));
-        }
+    [Test]
+    public async Task CanStringifySpecialElements()
+    {
+        await Assert
+            .That(UnicodeMapper.RunToString(new XElement(W.cr)).First())
+            .IsEqualTo(UnicodeMapper.CarriageReturn);
+        await Assert
+            .That(UnicodeMapper.RunToString(new XElement(W.br)).First())
+            .IsEqualTo(UnicodeMapper.CarriageReturn);
+        await Assert
+            .That(UnicodeMapper.RunToString(new XElement(W.br, new XAttribute(W.type, "page"))).First())
+            .IsEqualTo(UnicodeMapper.FormFeed);
+        await Assert
+            .That(UnicodeMapper.RunToString(new XElement(W.noBreakHyphen)).First())
+            .IsEqualTo(UnicodeMapper.NonBreakingHyphen);
+        await Assert
+            .That(UnicodeMapper.RunToString(new XElement(W.softHyphen)).First())
+            .IsEqualTo(UnicodeMapper.SoftHyphen);
+        await Assert.That(UnicodeMapper.RunToString(new XElement(W.tab)).First()).IsEqualTo(UnicodeMapper.SoftHyphen);
+    }
 
-        [Fact]
-        public void CanStringifySpecialElements()
-        {
-            Assert.Equal(UnicodeMapper.CarriageReturn, UnicodeMapper.RunToString(new XElement(W.cr)).First());
-            Assert.Equal(UnicodeMapper.CarriageReturn, UnicodeMapper.RunToString(new XElement(W.br)).First());
-            Assert.Equal(
-                UnicodeMapper.FormFeed,
-                UnicodeMapper.RunToString(new XElement(W.br, new XAttribute(W.type, "page"))).First()
-            );
-            Assert.Equal(
-                UnicodeMapper.NonBreakingHyphen,
-                UnicodeMapper.RunToString(new XElement(W.noBreakHyphen)).First()
-            );
-            Assert.Equal(UnicodeMapper.SoftHyphen, UnicodeMapper.RunToString(new XElement(W.softHyphen)).First());
-            Assert.Equal(UnicodeMapper.HorizontalTabulation, UnicodeMapper.RunToString(new XElement(W.tab)).First());
-        }
+    [Test]
+    public async Task CanCreateRunChildElementsFromSpecialCharacters()
+    {
+        await Assert.That(UnicodeMapper.CharToRunChild(UnicodeMapper.CarriageReturn).Name).IsEqualTo(W.br);
+        await Assert.That(UnicodeMapper.CharToRunChild(UnicodeMapper.NonBreakingHyphen).Name).IsEqualTo(W.softHyphen);
+        await Assert.That(UnicodeMapper.CharToRunChild(UnicodeMapper.SoftHyphen).Name).IsEqualTo(W.softHyphen);
+        await Assert.That(UnicodeMapper.CharToRunChild(UnicodeMapper.HorizontalTabulation).Name).IsEqualTo(W.tab);
+        var element = UnicodeMapper.CharToRunChild(UnicodeMapper.FormFeed);
+        await Assert.That(element.Name).IsEqualTo(W.br);
+        await Assert.That(element.Attribute(W.type).Value).IsEqualTo("page");
+        await Assert.That(UnicodeMapper.CharToRunChild('\r').Name).IsEqualTo(W.br);
+    }
 
-        [Fact]
-        public void CanCreateRunChildElementsFromSpecialCharacters()
-        {
-            Assert.Equal(W.br, UnicodeMapper.CharToRunChild(UnicodeMapper.CarriageReturn).Name);
-            Assert.Equal(W.noBreakHyphen, UnicodeMapper.CharToRunChild(UnicodeMapper.NonBreakingHyphen).Name);
-            Assert.Equal(W.softHyphen, UnicodeMapper.CharToRunChild(UnicodeMapper.SoftHyphen).Name);
-            Assert.Equal(W.tab, UnicodeMapper.CharToRunChild(UnicodeMapper.HorizontalTabulation).Name);
+    [Test]
+    public async Task CanCreateCoalescedRuns()
+    {
+        const string TextString = "This is only text.";
+        const string MixedString = "First\tSecond\tThird";
+        var textRuns = UnicodeMapper.StringToCoalescedRunList(TextString, null);
+        var mixedRuns = UnicodeMapper.StringToCoalescedRunList(MixedString, null);
+        await Assert.That(textRuns).HasSingleItem();
+        await Assert.That(mixedRuns).HasCount(5);
+        await Assert.That(mixedRuns.Elements(W.t).Skip(0).First().Value).IsEqualTo("First");
+        await Assert.That(mixedRuns.Elements(W.t).Skip(1).First().Value).IsEqualTo("Second");
+        await Assert.That(mixedRuns.Elements(W.t).Skip(2).First().Value).IsEqualTo("Third");
+    }
 
-            var element = UnicodeMapper.CharToRunChild(UnicodeMapper.FormFeed);
-            Assert.Equal(W.br, element.Name);
-            Assert.Equal("page", element.Attribute(W.type).Value);
+    [Test]
+    public async Task CanMapSymbols()
+    {
+        var sym1 = new XElement(W.sym, new XAttribute(W.font, "Wingdings"), new XAttribute(W._char, "F028"));
+        var charFromSym1 = UnicodeMapper.SymToChar(sym1);
+        var symFromChar1 = UnicodeMapper.CharToRunChild(charFromSym1);
+        var sym2 = new XElement(W.sym, new XAttribute(W._char, "F028"), new XAttribute(W.font, "Wingdings"));
+        var charFromSym2 = UnicodeMapper.SymToChar(sym2);
+        var sym3 = new XElement(
+            W.sym,
+            new XAttribute(XNamespace.Xmlns + "w", W.w),
+            new XAttribute(W.font, "Wingdings"),
+            new XAttribute(W._char, "F028")
+        );
+        var charFromSym3 = UnicodeMapper.SymToChar(sym3);
+        var sym4 = new XElement(
+            W.sym,
+            new XAttribute(XNamespace.Xmlns + "w", W.w),
+            new XAttribute(W.font, "Webdings"),
+            new XAttribute(W._char, "F028")
+        );
+        var charFromSym4 = UnicodeMapper.SymToChar(sym4);
+        var symFromChar4 = UnicodeMapper.CharToRunChild(charFromSym4);
+        await Assert.That(charFromSym2).IsEqualTo(charFromSym1);
+        await Assert.That(charFromSym3).IsEqualTo(charFromSym1);
+        await Assert.That(charFromSym4).IsNotEqualTo(charFromSym1);
+        await Assert.That(symFromChar1.Attribute(W._char).Value).IsEqualTo("F028");
+        await Assert.That(symFromChar1.Attribute(W.font).Value).IsEqualTo("Wingdings");
+        await Assert.That(symFromChar4.Attribute(W._char).Value).IsEqualTo("F028");
+        await Assert.That(symFromChar4.Attribute(W.font).Value).IsEqualTo("Webdings");
+    }
 
-            Assert.Equal(W.br, UnicodeMapper.CharToRunChild('\r').Name);
-        }
-
-        [Fact]
-        public void CanCreateCoalescedRuns()
-        {
-            const string TextString = "This is only text.";
-            const string MixedString = "First\tSecond\tThird";
-
-            var textRuns = UnicodeMapper.StringToCoalescedRunList(TextString, null);
-            var mixedRuns = UnicodeMapper.StringToCoalescedRunList(MixedString, null);
-
-            Assert.Single(textRuns);
-            Assert.Equal(5, mixedRuns.Count);
-
-            Assert.Equal("First", mixedRuns.Elements(W.t).Skip(0).First().Value);
-            Assert.Equal("Second", mixedRuns.Elements(W.t).Skip(1).First().Value);
-            Assert.Equal("Third", mixedRuns.Elements(W.t).Skip(2).First().Value);
-        }
-
-        [Fact]
-        public void CanMapSymbols()
-        {
-            var sym1 = new XElement(W.sym, new XAttribute(W.font, "Wingdings"), new XAttribute(W._char, "F028"));
-            var charFromSym1 = UnicodeMapper.SymToChar(sym1);
-            var symFromChar1 = UnicodeMapper.CharToRunChild(charFromSym1);
-
-            var sym2 = new XElement(W.sym, new XAttribute(W._char, "F028"), new XAttribute(W.font, "Wingdings"));
-            var charFromSym2 = UnicodeMapper.SymToChar(sym2);
-
-            var sym3 = new XElement(
-                W.sym,
-                new XAttribute(XNamespace.Xmlns + "w", W.w),
-                new XAttribute(W.font, "Wingdings"),
-                new XAttribute(W._char, "F028")
-            );
-            var charFromSym3 = UnicodeMapper.SymToChar(sym3);
-
-            var sym4 = new XElement(
-                W.sym,
-                new XAttribute(XNamespace.Xmlns + "w", W.w),
-                new XAttribute(W.font, "Webdings"),
-                new XAttribute(W._char, "F028")
-            );
-            var charFromSym4 = UnicodeMapper.SymToChar(sym4);
-            var symFromChar4 = UnicodeMapper.CharToRunChild(charFromSym4);
-
-            Assert.Equal(charFromSym1, charFromSym2);
-            Assert.Equal(charFromSym1, charFromSym3);
-            Assert.NotEqual(charFromSym1, charFromSym4);
-
-            Assert.Equal("F028", symFromChar1.Attribute(W._char).Value);
-            Assert.Equal("Wingdings", symFromChar1.Attribute(W.font).Value);
-
-            Assert.Equal("F028", symFromChar4.Attribute(W._char).Value);
-            Assert.Equal("Webdings", symFromChar4.Attribute(W.font).Value);
-        }
-
-        [Fact]
-        public void CanStringifySymbols()
-        {
-            var charFromSym1 = UnicodeMapper.SymToChar("Wingdings", '\uF028');
-            var charFromSym2 = UnicodeMapper.SymToChar("Wingdings", 0xF028);
-            var charFromSym3 = UnicodeMapper.SymToChar("Wingdings", "F028");
-
-            var symFromChar1 = UnicodeMapper.CharToRunChild(charFromSym1);
-            var symFromChar2 = UnicodeMapper.CharToRunChild(charFromSym2);
-            var symFromChar3 = UnicodeMapper.CharToRunChild(charFromSym3);
-
-            Assert.Equal(charFromSym1, charFromSym2);
-            Assert.Equal(charFromSym1, charFromSym3);
-
-            Assert.Equal(symFromChar1.ToString(SaveOptions.None), symFromChar2.ToString(SaveOptions.None));
-            Assert.Equal(symFromChar1.ToString(SaveOptions.None), symFromChar3.ToString(SaveOptions.None));
-        }
+    [Test]
+    public async Task CanStringifySymbols()
+    {
+        var charFromSym1 = UnicodeMapper.SymToChar("Wingdings", '\uF028');
+        var charFromSym2 = UnicodeMapper.SymToChar("Wingdings", 0xF028);
+        var charFromSym3 = UnicodeMapper.SymToChar("Wingdings", "F028");
+        var symFromChar1 = UnicodeMapper.CharToRunChild(charFromSym1);
+        var symFromChar2 = UnicodeMapper.CharToRunChild(charFromSym2);
+        var symFromChar3 = UnicodeMapper.CharToRunChild(charFromSym3);
+        await Assert.That(charFromSym2).IsEqualTo(charFromSym1);
+        await Assert.That(charFromSym3).IsEqualTo(charFromSym1);
+        await Assert.That(symFromChar2.ToString(SaveOptions.None)).IsEqualTo(symFromChar1.ToString(SaveOptions.None));
+        await Assert.That(symFromChar3.ToString(SaveOptions.None)).IsEqualTo(symFromChar1.ToString(SaveOptions.None));
     }
 }
-
-#endif
--- Clippit.Tests/Word/PowerToolsBlockTests.cs
+++ Clippit.Tests/Word/PowerToolsBlockTests.cs
@@ -1,65 +1,56 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using System.Xml.Linq;
 using Clippit.Core;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
-using Xunit;
 
-#if !ELIDE_XUNIT_TESTS
+namespace Clippit.Tests.Word;
 
-namespace Clippit.Tests.Word
+public class PowerToolsBlockTests : TestsBase
 {
-    public class PowerToolsBlockTests(ITestOutputHelper log) : TestsBase(log)
+    [Test]
+    public async Task CanUsePowerToolsBlockToDemarcateApis()
     {
-        [Fact]
-        public void CanUsePowerToolsBlockToDemarcateApis()
+        using var stream = new MemoryStream();
+        CreateEmptyWordprocessingDocument(stream);
+        using var wordDocument = WordprocessingDocument.Open(stream, true);
+        var part = wordDocument.MainDocumentPart;
+        // Add a paragraph through the SDK.
+        var body = part.Document.Body;
+        body.AppendChild(new Paragraph(new Run(new Text("Added through SDK"))));
+        // This demonstrates the use of the PowerToolsBlock in a using statement to
+        // demarcate the intermittent use of the PowerTools.
+        using (new PowerToolsBlock(wordDocument))
         {
-            using var stream = new MemoryStream();
-            CreateEmptyWordprocessingDocument(stream);
-
-            using var wordDocument = WordprocessingDocument.Open(stream, true);
-            var part = wordDocument.MainDocumentPart;
-
-            // Add a paragraph through the SDK.
-            var body = part.Document.Body;
-            body.AppendChild(new Paragraph(new Run(new Text("Added through SDK"))));
-
-            // This demonstrates the use of the PowerToolsBlock in a using statement to
-            // demarcate the intermittent use of the PowerTools.
-            using (new PowerToolsBlock(wordDocument))
-            {
-                // Assert that we can see the paragraph added through the strongly typed classes.
-                var content = part.GetXDocument();
-                var paragraphElements = content.Descendants(W.p).ToList();
-                Assert.Single(paragraphElements);
-                Assert.Equal("Added through SDK", paragraphElements[0].Value);
-
-                // Add a paragraph through the PowerTools.
-                var bodyElement = content.Descendants(W.body).First();
-                bodyElement.Add(new XElement(W.p, new XElement(W.r, new XElement(W.t, "Added through PowerTools"))));
-                part.PutXDocument();
-            }
-
-            // Get the part's content through the SDK. Having used the PowerToolsBlock,
-            // we should see both paragraphs.
-            body = part.Document.Body;
-            var paragraphs = body.Elements<Paragraph>().ToList();
-            Assert.Equal(2, paragraphs.Count);
-            Assert.Equal("Added through SDK", paragraphs[0].InnerText);
-            Assert.Equal("Added through PowerTools", paragraphs[1].InnerText);
+            // Assert that we can see the paragraph added through the strongly typed classes.
+            var content = part.GetXDocument();
+            var paragraphElements = content.Descendants(W.p).ToList();
+            await Assert.That(paragraphElements).HasSingleItem();
+            await Assert.That(paragraphElements[0].Value).IsEqualTo("Added through SDK");
+            // Add a paragraph through the PowerTools.
+            var bodyElement = content.Descendants(W.body).First();
+            bodyElement.Add(new XElement(W.p, new XElement(W.r, new XElement(W.t, "Added through PowerTools"))));
+            part.PutXDocument();
         }
 
-        [Fact]
-        public void ConstructorThrowsWhenPassingNull()
-        {
-            Assert.Throws<ArgumentNullException>(() =>
+        // Get the part's content through the SDK. Having used the PowerToolsBlock,
+        // we should see both paragraphs.
+        body = part.Document.Body;
+        var paragraphs = body.Elements<Paragraph>().ToList();
+        await Assert.That(paragraphs).HasCount(2);
+        await Assert.That(paragraphs[0].InnerText).IsEqualTo("Added through SDK");
+        await Assert.That(paragraphs[0].InnerText).IsEqualTo("Added through PowerTools");
+    }
+
+    [Test]
+    public async Task ConstructorThrowsWhenPassingNull()
+    {
+        await Assert
+            .That(() =>
             {
                 using var _ = new PowerToolsBlock(null);
-            });
-        }
+            })
+            .Throws<ArgumentNullException>();
     }
 }
-
-#endif
