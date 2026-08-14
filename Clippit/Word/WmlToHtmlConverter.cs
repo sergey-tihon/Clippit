a26a72f9e7a84ad0f0682077b23f8097df1e104b
alexeysp11 <alexeyspiridon1111@gmail.com>
2026-08-14 06:52:34 +0500

    nullable return value

--- Clippit/Word/WmlToHtmlConverter.cs
+++ Clippit/Word/WmlToHtmlConverter.cs
@@ -45,7 +45,7 @@ namespace Clippit.Word
         public string AdditionalCss;
         public bool RestrictToSupportedLanguages;
         public bool RestrictToSupportedNumberingFormats;
-        public Dictionary<string, Func<int, NumberingFormatType, string>> ListItemImplementations;
+        public Dictionary<string, Func<int, NumberingFormatType, string?>> ListItemImplementations;
         public Func<ImageInfo, XElement> ImageHandler;
 
         public WmlToHtmlConverterSettings()
@@ -84,7 +84,7 @@ namespace Clippit.Word
         public string AdditionalCss = "";
         public bool RestrictToSupportedLanguages = false;
         public bool RestrictToSupportedNumberingFormats = false;
-        public Dictionary<string, Func<int, NumberingFormatType, string>> ListItemImplementations =
+        public Dictionary<string, Func<int, NumberingFormatType, string?>> ListItemImplementations =
             ListItemRetrieverSettings.DefaultListItemTextImplementations;
         public Func<ImageInfo, XElement> ImageHandler;
     }
